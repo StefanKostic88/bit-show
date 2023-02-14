b839d27cd8b9c8595fe44c37b672f29c0b68b1c5
StefanKostic88 <stefankostic88@yahoo.com>
2023-02-14 18:41:11 +0100

    logo handler added

--- JS/controller.js
+++ JS/controller.js
@@ -18,11 +18,35 @@
 
     $(".form-control").on("keyup", controlInputChange);
     $(".pagination").on("click", controlPagination);
-    $(".logo").on("click", initLandingPage);
+    $(".logo").on("click", controlLogoClick);
     $(".form-control").on("mouseup", controlClearMovieList);
   };
 
+  const initLandingPage = async function () {
+    try {
+      const firstPage = [...(await getInitialData())];
+      MovieCardView.render(firstPage);
+      PaginationView.render(state.currentPage);
+      $(".card")
+        .fadeOut(0)
+        .delay(100)
+        .fadeIn(500)
+        .on("click", controlMovieInfo);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
   ////controlerHandlers
+
+  const controlLogoClick = async function () {
+    const firstPage = [...(await getInitialData())];
+    MovieCardView.render(firstPage);
+    state.currentPage = changePage(0);
+    PaginationView.render(state.currentPage);
+    PaginationView.showPagination();
+    $(".card").fadeOut(0).delay(100).fadeIn(500).on("click", controlMovieInfo);
+  };
 
   const controlPagination = async function (e) {
     if (typeof e === "object") {
@@ -54,33 +78,18 @@
     $("#close-movie__info").on("click", controlCloseMovieInfo);
   };
 
+  const controlInputChange = async function () {
+    const movieListArr = [...(await getSearchMovieLIstData(this.value))];
+    SerachMovieListView.render(movieListArr);
+    $(".search-movie__list-item").on("click", controlMovieInfo);
+  };
+
   const controlClearMovieList = function () {
     setTimeout(() => {
       if ($(this).val() === "") {
         SerachMovieListView.clearParentContainer();
       }
     }, 200);
-  };
-
-  const initLandingPage = async function () {
-    try {
-      const firstPage = [...(await getInitialData())];
-      MovieCardView.render(firstPage);
-      PaginationView.render(state.currentPage);
-      $(".card")
-        .fadeOut(0)
-        .delay(100)
-        .fadeIn(500)
-        .on("click", controlMovieInfo);
-    } catch (err) {
-      console.log(err);
-    }
-  };
-
-  const controlInputChange = async function () {
-    const movieListArr = [...(await getSearchMovieLIstData(this.value))];
-    SerachMovieListView.render(movieListArr);
-    $(".search-movie__list-item").on("click", controlMovieInfo);
   };
 
   /// Initalization
